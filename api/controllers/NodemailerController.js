a04c45061a5b6d9068d79805abaf4a78d4fa3a19
jeff <oghenerukevwejeff@gmail.com>
2018-02-02 15:10:25 +0000

    found my database in mlab

--- api/controllers/NodemailerController.js
+++ api/controllers/NodemailerController.js
@@ -48,10 +48,9 @@ module.exports = {
                 if (error) {
                     return console.log(error);
                 }
-                console.log('Message sent: %s', info.messageId);
+                res.json({Messagesent:info.messageId});
                 // Preview only available when sending through an Ethereal account
-                console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
+                res.json({PreviewURL:nodemailer.getTestMessageUrl(info)});
                 // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
                 // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
             });
