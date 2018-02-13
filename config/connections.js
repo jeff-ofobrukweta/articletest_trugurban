5ba40ba36a305d2ef1c4152d7a6f226b62be7eec
jeff <oghenerukevwejeff@gmail.com>
2018-02-13 02:45:49 +0000

    newest update

--- config/connections.js
+++ config/connections.js
@@ -56,19 +56,19 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  someMongodbServer: {
-    adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    user: '', //optional
-    password: '', //optional
-    database: 'truggurbandatabase' //optional
-  },
   // someMongodbServer: {
   //   adapter: 'sails-mongo',
-  //   url: 'mongodb://heroku_8r1032sz:trs53st91cdc38s6ec748dkqve@ds161304.mlab.com:61304/heroku_8r1032sz',
-  //   schema:true
+  //   host: 'localhost',
+  //   port: 27017,
+  //   user: '', //optional
+  //   password: '', //optional
+  //   database: 'truggurbandatabase' //optional
   // },
+  someMongodbServer: {
+    adapter: 'sails-mongo',
+    url: 'mongodb://heroku_8r1032sz:trs53st91cdc38s6ec748dkqve@ds161304.mlab.com:61304/heroku_8r1032sz',
+    schema:true
+  },
   //mongodb://<dbuser>:<dbpassword>@ds139430.mlab.com:39430/olando
   /***************************************************************************
   *                                                                          *
