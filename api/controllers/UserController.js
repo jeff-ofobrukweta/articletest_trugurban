2a49e812237384e335d3ec381faa5cd817c24067
jeff <oghenerukevwejeff@gmail.com>
2018-02-26 14:49:10 +0000

    forgot password

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -118,7 +118,7 @@ module.exports = {
     // const password = req.body.password;
     //this check if the user input is undefined or empty
     if (email==="") {
-      return res.badRequest('An email address is required!');
+      return res.json('An email address is required!');
       }
     //find if the user exists in the database
     User.findOne({
